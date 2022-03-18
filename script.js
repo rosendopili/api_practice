5cd67b0a9eae11f6461edc878327ae442ba25b38
rosendopili <rosendopili@gmail.com>
2022-03-18 18:58:49 -0400

    more changes

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ const temp = document.querySelector('.temp');
 const description = document.querySelector('.description');
 const low = document.querySelector('.low');
 const high = document.querySelector('.high');
-const weatherContainer = document.querySelector('.weather_container');
 const zipcode = document.querySelector('.zipcode'); 
 const submit = document.querySelector('.submit')
 const hat = document.querySelector('.hat'); 
 const shirt = document.querySelector('.shirt');
 const pants= document.querySelector('.pants');
 const shoes = document.querySelector('.shoes'); 
-const suggestionContainer = document.querySelector('.suggestion_container'); 
 const contentContainer = document.querySelector('.content_container'); 
+const date = new Date()
+const time = date.getHours()
 
 const submitZip = () => {
     let zip = zipcode.value
@@ -23,10 +23,9 @@ const submitZip = () => {
             return response.json(); 
         })
         .then((response) => {
-            console.log(response)
             contentContainer.classList.remove('display_none'); 
             getWeather(response); 
-            zipcode.value = ""; 
+            zipcode.value = ''; 
             getSuggestions(response); 
         })
         .catch((error) => {
@@ -35,32 +34,28 @@ const submitZip = () => {
 }; 
 
 const getWeather = (response) => {
+    loadWeatherIcon(response); 
 
     cityname.textContent = response.name;
-if(response.weather[0].main = 'clouds'){
-    description.setAttribute('src', 'assets/day_time/scatter_broken_clouds.png');
-}
-
     temp.textContent = `${Math.floor(response.main.temp)}˚`;
     low.textContent = `${Math.floor(response.main.temp_min)}˚`;
     high.textContent = `${Math.floor(response.main.temp_max)}˚`;
 
-    let lowLabel = document.createElement('p');
-    lowLabel.textContent = 'Low'; 
-    lowLabel.classList.add('label')
-    low.appendChild(lowLabel); 
+        let lowLabel = document.createElement('p');
+            lowLabel.textContent = 'Low'; 
+            lowLabel.classList.add('label')
+            low.appendChild(lowLabel); 
 
-    let highLabel = document.createElement('p');
-    highLabel.textContent = 'High'; 
-    highLabel.classList.add('label')
-    high.appendChild(highLabel); 
-
-
+        let highLabel = document.createElement('p');
+            highLabel.textContent = 'High'; 
+            highLabel.classList.add('label')
+            high.appendChild(highLabel); 
 }
 
 const getSuggestions = (response) => {
 
-    pants.setAttribute('src', 'assets/clothing/joggers.png'); 
+    pants.setAttribute('src', 'assets/clothing/joggers.png');
+       
     if (response.main.temp_max > 50) {
         hat.setAttribute('src', 'assets/clothing/fitted.png');
         shirt.setAttribute('src', 'assets/clothing/tshirt.png');
@@ -72,6 +67,30 @@ const getSuggestions = (response) => {
     }
 }
 
+const loadWeatherIcon = (response) => {   
+
+    let folder = (time >= 19 || time < 6) ? 'night_time':'day_time';
+
+    switch(response.weather[0].main){
+        case 'Clouds':
+            description.setAttribute('src', `assets/${folder}/few_clouds.png`);
+            break; 
+        case 'Clear':
+            description.setAttribute('src', `assets/${folder}/clear_sky.png`);
+            break; 
+        case 'Rain':
+            description.setAttribute('src', 'assets/day_time/heavy_rain.png');
+            break; 
+        case 'Drizzle':
+            description.setAttribute('src', 'assets/day_time/rain.png');
+            break;     
+        case 'Thunderstorm':
+            description.setAttribute('src', 'assets/day_time/thunderstorm.png');
+            break;
+        default: description.setAttribute('src', 'assets/day_time/mist.png');
+
+    }
+}
 
 submit.addEventListener('click', (e) => {
     e.preventDefault()
